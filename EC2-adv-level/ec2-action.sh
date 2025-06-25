01ead16cd6a9617704c6d8f9b82eedbbacf7ed0d
Sumit S Purandare <122196568+SumitSP404@users.noreply.github.com>
2025-06-25 14:04:27 +0530

    Update ec2-action.sh

--- EC2-adv-level/ec2-action.sh
+++ EC2-adv-level/ec2-action.sh
@@ -3,7 +3,7 @@
 ACTION=$1  # notify or stop
 
 # Configuration
-INSTANCE_ID="i-0123456789abcdef0"
+INSTANCE_ID="i-0cc2a03553d181551"
 TOPIC_ARN="arn:aws:sns:ap-south-1:458799594567:ec2-alert-testing"
 REGION="ap-south-1"
 
